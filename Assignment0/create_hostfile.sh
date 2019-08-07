4336a3a766e2576d5c15fd0f74da3a6defa9a590
Mayank Sharma <mayank8019@gmail.com>
2019-08-07 18:13:15 +0530

    Initial commit with Assignment0

---
+++ Assignment0/create_hostfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+then
+	echo "This script takes a single argument - the number of processes to run on a single node."
+	exit -1
+fi
+
+n_proc=$1
+hostfile_name="mpich.hostfile"
+
+check_host () {
+	local run=$1
+	ping -c1 csews${1} 2>&1 > /dev/null
+	if [ $? -eq 0 ]
+	then
+		echo "csews${1}:$n_proc" | tee -a ${hostfile_name} 
+	fi
+}
+
+echo "# This is an automatically created hostfile using create_hostfile.sh script" > ${hostfile_name}
+for i in `seq 50`; do check_host ${i} & done
+
---
+++ Assignment0/ssh_pwdless.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# Navigatge to HOME directory (if not already in)
+cd ~
+# Generate public/private RSA key
+echo "StrictHostKeyChecking no" >> config
+# Reset all hosts to connect password-less henceforth
+rm -f known_hosts*
+# Add all nodes in cluster to known_hosts
+for i in `seq 1 50`
+do
+ssh csews$i uptime
+done
