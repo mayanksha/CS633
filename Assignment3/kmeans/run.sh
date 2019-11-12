896780854115dfec0af17528939dc4249ae57ba4
Mayank Sharma <mayanksha@cse.iitk.ac.in>
2019-11-12 22:43:55 +0530

    Fixed run.sh script for cse clusters

--- Assignment3/kmeans/run.sh
+++ Assignment3/kmeans/run.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# set -x
+set -x
 # data_sizes=(65536)
 # Remove the older data files
 make clean 2>&1 > /dev/null
-
-./create_hostfile.sh 12
-if [ $? -ne 0 ]
-then
-    echo "Some problem with create the hostfile! Exiting!"
-    exit -1
-fi
 
 # Compile the src.c file
 make -f "Makefile"
@@ -21,25 +14,42 @@ then
 fi
 
 mkdir -p cse
-dataset_1_files=`ls ./data1/file*`
-dataset_2_files=`ls ./data2/file*`
+rm -rf cse/*
+dataset_1_files=($(ls ./data1/file*))
+dataset_2_files=($(ls ./data2/file*))
 
 n_procs=(1 2 3 4 5 6 7 8 9 10)
 n_hosts=(1 2 3)
-KVal="10"
+KVal="90"
 # Truncate the data file
 for i in "${n_hosts[@]}"
 do
     for j in "${n_procs[@]}"
     do
-        total_proc=$(( $i * $j ))\n
+        total_proc=$(( $i + $j ))
+        temp_out_file="cse/temp_output_${total_proc}.txt"
         out_file="cse/output_${total_proc}.txt"
+        printf "" > "$temp_out_file"
         printf "" > "$out_file"
+
         for k in "${dataset_1_files[@]}"
         do
-            mpiexec -f mpich.hostfile -np "${total_proc}" -ppn "${j}" ./src.x "${k}" ${KVal} \
-                >> "${out_file}"
+            echo "${k}"
+            mpiexec -f mpich.hostfile -np "${total_proc}" -ppn "${j}" ./src.x "${k}" ${KVal} >> "${temp_out_file}"
         done
+
+        head -n1 "${temp_out_file}" >> "${out_file}"
+        sed -n '5~5p' "${temp_out_file}" >> "${out_file}"
+        total_times=($(sed -n '2~5p' ${temp_out_file} | grep -oP ' \K.*'))
+        total_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${total_times[@]}")
+        proc_times=($(sed -n '3~5p' ${temp_out_file} | grep -oP ' \K.*'))
+        proc_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${proc_times[@]}")
+        pre_proc_times=($(sed -n '4~5p' ${temp_out_file} | grep -oP ' \K.*'))
+        pre_proc_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${pre_proc_times[@]}")
+
+        echo "Total Time: ${total_avg}">> "${out_file}"
+        echo "Processing Time: ${proc_avg}">> "${out_file}"
+        echo "Pre-Processing Time: ${pre_proc_avg}">> "${out_file}"
     done
 done
 exit 0
