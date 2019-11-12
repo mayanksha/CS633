e745ae9a8728ffc647ba01b3bfcf89f6a183152a
Mayank Sharma <mayanksha@cse.iitk.ac.in>
2019-11-13 04:40:09 +0530

    Finished run.sh script for cse cluster

--- Assignment3/kmeans/run.sh
+++ Assignment3/kmeans/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -x
+set -e
 # data_sizes=(65536)
 # Remove the older data files
 make clean 2>&1 > /dev/null
@@ -13,43 +14,56 @@ then
     exit -1
 fi
 
-mkdir -p cse
 rm -rf cse/*
-dataset_1_files=($(ls ./data1/file*))
-dataset_2_files=($(ls ./data2/file*))
+data_folders=("data1" "data2")
 
-n_procs=(1 2 3 4 5 6 7 8 9 10)
+n_procs=(1 2 3 4 8 12)
 n_hosts=(1 2 3)
-KVal="90"
 # Truncate the data file
-for i in "${n_hosts[@]}"
+
+for data_folder in "${data_folders[@]}"
 do
-    for j in "${n_procs[@]}"
+    KVal=""
+    if [ "${data_folder}" = "data1" ]
+    then
+        KVal="90"
+    else
+        KVal="11"
+    fi
+
+    dataset_files=($(ls ./${data_folder}/file*))
+    for i in "${n_hosts[@]}"
     do
-        total_proc=$(( $i + $j ))
-        temp_out_file="cse/temp_output_${total_proc}.txt"
-        out_file="cse/output_${total_proc}.txt"
-        printf "" > "$temp_out_file"
-        printf "" > "$out_file"
-
-        for k in "${dataset_1_files[@]}"
+        for j in "${n_procs[@]}"
         do
-            echo "${k}"
-            mpiexec -f mpich.hostfile -np "${total_proc}" -ppn "${j}" ./src.x "${k}" ${KVal} >> "${temp_out_file}"
+            total_proc=$(( $i * $j ))
+            mkdir -p "./cse/${data_folder}/"
+            temp_out_file="./cse/${data_folder}/temp_output_${total_proc}.txt"
+            out_file="./cse/${data_folder}/output_${total_proc}.txt"
+            printf "" > "$temp_out_file"
+            printf "" > "$out_file"
+            echo "$temp_out_file"
+            echo "$out_file"
+
+
+            for k in "${dataset_files[@]}"
+            do
+                mpiexec -f mpich.hostfile -np "${total_proc}" -ppn "${j}" ./src.x "${k}" ${KVal} >> "${temp_out_file}"
+            done
+
+            head -n1 "${temp_out_file}" >> "${out_file}"
+            sed -n '5~5p' "${temp_out_file}" >> "${out_file}"
+            total_times=($(sed -n '2~5p' ${temp_out_file} | grep -oP ' \K.*'))
+            total_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${total_times[@]}")
+            proc_times=($(sed -n '3~5p' ${temp_out_file} | grep -oP ' \K.*'))
+            proc_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${proc_times[@]}")
+            pre_proc_times=($(sed -n '4~5p' ${temp_out_file} | grep -oP ' \K.*'))
+            pre_proc_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${pre_proc_times[@]}")
+
+            echo "Total Time: ${total_avg}">> "${out_file}"
+            echo "Processing Time: ${proc_avg}">> "${out_file}"
+            echo "Pre-Processing Time: ${pre_proc_avg}">> "${out_file}"
         done
-
-        head -n1 "${temp_out_file}" >> "${out_file}"
-        sed -n '5~5p' "${temp_out_file}" >> "${out_file}"
-        total_times=($(sed -n '2~5p' ${temp_out_file} | grep -oP ' \K.*'))
-        total_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${total_times[@]}")
-        proc_times=($(sed -n '3~5p' ${temp_out_file} | grep -oP ' \K.*'))
-        proc_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${proc_times[@]}")
-        pre_proc_times=($(sed -n '4~5p' ${temp_out_file} | grep -oP ' \K.*'))
-        pre_proc_avg=$(python -c  'import sys; print(sum([float(i) for i in sys.argv[1:]])/len(sys.argv[1:]))' "${pre_proc_times[@]}")
-
-        echo "Total Time: ${total_avg}">> "${out_file}"
-        echo "Processing Time: ${proc_avg}">> "${out_file}"
-        echo "Pre-Processing Time: ${pre_proc_avg}">> "${out_file}"
     done
 done
 exit 0
